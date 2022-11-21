7d3a76f504ca60a31b28b3a131544e5113299335
Rafael Lottermann <rgarus@kopp.com.br>
2022-11-21 19:46:43 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/fluxo_maximo_otimizacao_algoritmo/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.fluxo_maximo_otimizacao_algoritmo;
+
+/**
+ *
+ * @author rafael
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+    }
+    
+}
